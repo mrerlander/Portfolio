87cc0ca5ad5634da1e89672166f19b89e7112dd2
Michael Erlander <mrerlander@gmail.com>
2021-01-12 11:41:37 -0600

    removed email js in favor of mailto

--- assets/app.js
+++ assets/app.js
@@ -8,14 +8,11 @@ $(document).on("submit", "#contact-form", function(event){
 
 event.preventDefault();
 
-  name = $("#first_name").val().trim() + " " + $("#last_name").val().trim();
+  userName = $("#first_name").val().trim() + " " + $("#last_name").val().trim();
   message = $("#message").val().trim();
   company = $("#company").val().trim();
   emailAdd = $("#email").val().trim();
 
-
-  emailjs.send("gmail","portfolio",{from_name: name, message: message, company_name: company, reply_to: emailAdd});
-
-  $("#form-sub").text("Email Sent");
-  $("#form-sub").prop("disabled", true);
+  window.location.href = "mailto:" + emailAdd + "?subject=" + company + "&body=" + message;
+  
 });
